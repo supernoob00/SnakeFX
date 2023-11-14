c069e5e9c7d90bd49ce2ffbe3325a675dd910ede
Sam Somerdin <sjsomerdin@gmail.com>
2023-11-14 10:12:41 -0500

    Created fat JAR

---
+++ src/main/java/com/somerdin/snake/Launcher.java
@@ -0,0 +1,7 @@
+package com.somerdin.snake;
+
+public class Launcher {
+    public static void main(String[] args) {
+        SnakeApplication.main(args);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,9 @@
 module com.somerdin.snake {
     requires javafx.controls;
-    requires javafx.fxml;
     requires javafx.media;
 
 
-    opens com.somerdin.snake to javafx.fxml;
     exports com.somerdin.snake;
     exports com.somerdin.snake.Point;
-    opens com.somerdin.snake.Point to javafx.fxml;
     exports com.somerdin.snake.Resource;
-    opens com.somerdin.snake.Resource to javafx.fxml;
 }
